4a3c6ccbb3da87948e07fc5a65a7718b2345b437
Karl E. Jørgensen <karl.jorgensen@recordsure.com>
2023-12-08 23:33:48 +0000

    Simplification and pylinting

--- Makefile
+++ Makefile
@@ -4,6 +4,10 @@ export KUBECTL_CONTEXT=home
 export KUBECTL_NAMESPACE=kj
 
 .PHONY: run
-run :
+run : pylint
 	# . bin/activate; kopf run --all-namespaces --dev stupidlb.py
 	. bin/activate; kopf run --namespace kj --dev stupidlb.py
+
+.PHONY: pylint
+pylint :
+	. bin/activate; pylint stupidlb.py
--- stupidlb.py
+++ stupidlb.py
@@ -1,18 +1,35 @@
 #!/usr/bin/env python3
 #
 # https://kopf.readthedocs.io/en/stable/index.html
+"""Simple Stupid Load Balancer Operator
 
-import threading
-from types import SimpleNamespace
+This will assign external IPs and LoadbalancerIPs on services which do
+not have them from a pool of IP addresses.
+
+This will not _actually_ create an external load balancer; it is
+assumed that the IP addresses in the pool are valid for the local
+network (and wont conflict with anything else - e.g. DHCP-assigned
+addresses).
+
+"""
+
 import functools
+import logging
+import threading
 
 import kopf
 import kubernetes
-import logging
 
 MYNAME = 'stupidlb.jorgensen.org.uk'
 
+@functools.cache
 def valid_ips():
+    """Return a set of valid IP addresses we can pick from
+
+    This is the total set of IP addresses reserved for the operator to
+    assign from.
+
+    """
     return {'192.168.0.' + str(x)
             for x in range(224,240)}
 
@@ -20,6 +37,10 @@ LOCK = threading.Lock()
 
 @kopf.on.startup()
 def configure(settings: kopf.OperatorSettings, **_):
+    """Startup hook
+
+    See https://kopf.readthedocs.io/en/stable/startup/
+    """
     settings.persistence.progress_storage = kopf.AnnotationsProgressStorage(
         prefix=MYNAME)
     settings.persistence.diffbase_storage = kopf.AnnotationsDiffBaseStorage(
@@ -34,7 +55,19 @@ def k8s():
     return kubernetes.client.CoreV1Api()
 
 def is_interesting(meta, spec, **_):
+    """Whether we want to handle a given k8s resource
+
+    We only deal with LoadBalancer type services.
+
+    The user may annotate a service to be ignored by the operator.
+
+    We only care about services without IP addresses. Thus if the user
+    manually sets an IP address, we do not override it.
+
+    """
     if spec.get('type') != 'LoadBalancer':
+        return False
+    if meta.get('annotations', {}).get(MYNAME) == 'ignore':
         return False
     if meta.get('annotations', {}).get(MYNAME + '/kopf-managed', 'yes') == 'no':
         return False
@@ -44,7 +77,7 @@ def is_interesting(meta, spec, **_):
 
 @kopf.on.create('Service', when=is_interesting)
 @kopf.on.update('Service', when=is_interesting)
-def handle_service(meta, spec, logger, patch, **kwargs):
+def handle_service(meta, spec, logger, patch, **_):
     """Assign an external IP to a service"""
 
     # Without locking, things may fail if multiple services need to be
@@ -53,7 +86,6 @@ def handle_service(meta, spec, logger, patch, **kwargs):
         eips = spec.get('externalIPs', [])
         if not eips:
             eips = [pick_external_ip(meta, spec)]
-            patch.setdefault('spec', {})
             patch.spec['externalIPs'] = eips
             logger.info(f'Assigned external IP {eips[0]}')
 
@@ -65,30 +97,35 @@ def handle_service(meta, spec, logger, patch, **kwargs):
 
 def pick_external_ip(meta, spec):
     """Find a free IP address and return it"""
-    services = list(eips_in_use(exclude_ns=meta.namespace,
-                                exclude_name=meta.name))
-    services_dict = {
-        svc.ip: svc.namespace + '/' + svc.name
-        for svc in services
-    }
+    used = set(ips_in_use(exclude_ns=meta.namespace,
+                          exclude_name=meta.name))
+    print(f'{used=}')
 
-    avail = valid_ips() - set([svc.ip for svc in services])
+    avail = valid_ips() - used
     if not avail:
-        raise kopf.TemporaryError(f'No free IPs',
+        raise kopf.TemporaryError('No free IPs',
                                   delay=60)
-    # print(f'available IPs: {avail}')
+    print(f'{avail=}')
 
     lbip = spec.get('loadBalancerIP')
     if lbip:
-        if lbip not in avail:
-            raise kopf.TemporaryError(f'Cannot use load balancer IP {lbip} - it is in use by {services_dict[lbip]}',
-                                      delay=60)
-        return lbip
+        if lbip in avail:
+            return lbip
+
+        if lbip not in valid_ips():
+            raise kopf.TemporaryError(
+                f'loadBalancerIP {lbip} is not valid with current {MYNAME} config',
+                delay=60)
+
+        raise kopf.TemporaryError(f'loadBalancerIP {lbip} is valid but already in use',
+                                  delay=60)
 
     return avail.pop()
 
-def eips_in_use(exclude_ns:str=None, exclude_name:str=None):
-    """Figure out which eips are in use"""
+def ips_in_use(exclude_ns:str=None, exclude_name:str=None):
+    """Figure out which IP addresses are in use elsewhere
+
+    """
     for service in k8s().list_service_for_all_namespaces(watch=False).items:
         if service.spec.type != 'LoadBalancer':
             continue
@@ -97,12 +134,7 @@ def eips_in_use(exclude_ns:str=None, exclude_name:str=None):
             continue
 
         if service.spec.load_balancer_ip:
-            yield SimpleNamespace(namespace=service.metadata.namespace,
-                                  name=service.metadata.name,
-                                  ip=service.spec.load_balancer_ip)
+            yield service.spec.load_balancer_ip
 
         if service.spec.external_i_ps:
-            for eip in service.spec.external_i_ps:
-                yield SimpleNamespace(namespace=service.metadata.namespace,
-                                      name=service.metadata.name,
-                                      ip=eip)
+            yield from service.spec.external_i_ps
